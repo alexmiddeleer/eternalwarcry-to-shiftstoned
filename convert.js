385ee6a6681ff908ed933df1815dd45486084b77
alexMA <almiddeleer@gmail.com>
2019-01-22 23:21:15 -0500

    generating good import code

--- convert.js
+++ convert.js
@@ -1,18 +1,71 @@
 (function() {
+
+  function encodeValues(values) {
+    var code, error;
+
+    code = '';
+    for (var j = 0; j < values.length; j++) {
+      var i = values[j];
+      var r;
+
+      if (error) {
+        return;
+      }
+
+      if (i < 0) {
+        error = 'negative value';
+        return;
+      }
+
+      if (i === 0) {
+        code = code + 'A';
+      }
+
+      while (i > 0) {
+        r = i % 32;
+        i = (i - r) / 32;
+
+        if (i > 0) {
+          r = r + 32;
+        }
+
+        if (r < 26) {
+          code = code + String.fromCharCode(65 + r);
+        } else if (r < 52) {
+          code = code + String.fromCharCode(97 + r - 26);
+        } else if (r < 62) {
+          code = code + String.fromCharCode(48 + r - 52);
+        } else if (r === 62) {
+          code = code + '-';
+        } else if (r === 63) {
+          code = code + '_';
+        }
+      }
+    }
+
+    if (error) {
+      return null;
+    }
+
+    return code;
+  }
+
   var exportSplit = document.getElementById('export-deck-text').innerHTML.split('\n');
   var regex = /^(\d+) .+Set(\d+) #(\d+).$/;
   var marketTriplet = [2, 0, 0];
-  var triplets = [];
+  var tripletsArray = [];
   for (var i = 0; i < exportSplit.length; i++) {
     var matches = exportSplit[i].match(regex);
     if (!matches) {
       if (exportSplit[i].indexOf('MARKET') > 0) {
-        triplets.push(marketTriplet);
+        tripletsArray = tripletsArray.concat(marketTriplet);
       }
     } else {
-      var triplet = [matches[1], matches[2], matches[3]];
-      triplets.push(triplet);
+      var triplet = [parseInt(matches[1], 10), parseInt(matches[2], 10), parseInt(matches[3], 10)];
+      tripletsArray = tripletsArray.concat(triplet);
     }
   }
-  console.log(triplets);
+  console.log(exportSplit);
+  console.log(tripletsArray);
+  console.log(encodeValues(tripletsArray));
 })();
